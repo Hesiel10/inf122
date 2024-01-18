0b2137b0af7f272381014da19c7ce393d4c6e4c2
Hesiel10 <135052811+Hesiel10@users.noreply.github.com>
2024-01-18 04:19:31 +0000

    ejercicio4

---
+++ practica/ejercicio4/ejercicio-4/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Bar from './components/Bar';
+import ProgressBar from './components/ProgressBar';
+
+function App() {
+  return (
+    <div className="App">
+      <Bar/>
+      <ProgressBar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ practica/ejercicio4/ejercicio-4/src/components/Bar.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import '../styles/Bar.css';
+import ProgressBar from "./ProgressBar";
+
+function Bar() {
+  const [val, setVal] = useState(10);
+  const setValuer = (e) => setVal(Number(e.target.value));
+
+  return (
+    <>
+      <div className="App">
+        <h1>Progress bar</h1>
+        <ProgressBar width={val} />
+        <form>
+          <label for="html">Input Percentage:</label>
+          <input type="number" onChange={setValuer} />
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default Bar;
