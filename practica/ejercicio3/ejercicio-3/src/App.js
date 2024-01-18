736cc012c28947b863e3bf7d1ee90ba6e5d6c0ba
Hesiel10 <135052811+Hesiel10@users.noreply.github.com>
2024-01-18 03:33:21 +0000

    ejercicio3

---
+++ practica/ejercicio3/ejercicio-3/src/App.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+import Añadir from './components/Añadir';
+
+function App() {
+  return (
+    <div className="App">
+      <Añadir/>
+    </div>
+  );
+}
+
+export default App;
